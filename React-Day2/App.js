0841a183a7dfd76f693270571d762c46cffdf1a0
sourav3444 <dasgopal8538@gmail.com>
2023-02-24 17:44:12 +0530

    day 2

---
+++ React-Day2/App.js
@@ -0,0 +1,33 @@
+/*
+*
+*
+*
+*/
+
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading=React.createElement("h1",
+{
+    id:"title",
+}
+,"Heading 1");
+const heading2=React.createElement("h2",
+{
+    id:"title",
+},
+"Heading 2");
+
+//jakhon more than 1 element react element a run korate chai then by array[] pass korte hoi
+const container=React.createElement("div",
+{
+    id:"container",
+    hello:"world", //props can be anything in react
+},
+[heading,heading2]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+//passing the react element inside the root
+root.render(container);
+
